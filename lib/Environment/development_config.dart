190f4dde1520de87bd8832351aee70347dddf651
Tanuja Patil <tanuja@eyvo.com>
2025-08-12 18:11:51 +0530

    roation and order list changes

--- lib/Environment/development_config.dart
+++ lib/Environment/development_config.dart
@@ -2,7 +2,8 @@ import 'package:eyvo_inventory/Environment/base_configuration.dart';
 
 class DevelopmentConfiguration implements BaseConfig {
   @override
-  String get apiHost => 'https://service.eyvo.net/eBA API 2.0';
+//  String get apiHost => 'https://service.eyvo.net/eBA API 2.0';
+  String get apiHost => 'http://10.0.2.2:5213';
 
   @override
   String get domainHost => '';
